20a4fe20ad0f84c5a02e7ef6d394832403edfadb
Riccardo Coccioli <rcoccioli@wikimedia.org>
2025-06-30 10:54:17 +0200

    CSP: fix headers when loading local resources
    * When loading local resources the CSP headers for style and script are
      written by the django-csp extension, not inheriting anymore from
      default-src. Add explicitely 'self' to them.
    
    Bug: T397696
    Change-Id: Iedc4459e27837acf574e7935a2dbf89fe96888fa

--- debmonitor/debmonitor/settings/base.py
+++ debmonitor/debmonitor/settings/base.py
@@ -222,12 +222,16 @@ elif DEBMONITOR_CONFIG.get('LDAP', {}):
 
 # Content-Security-Policy
 CSP_DEFAULT_SRC = ("'self'")
+# This will generate script-src and style-src CSP headers so they will not inherit from default-src
+CSP_INCLUDE_NONCE_IN = ("script-src", "style-src")
 # Define the specific config only when differing from default-src
 if DEBMONITOR_JAVASCRIPT_STORAGE != "Debian":
     CSP_SCRIPT_SRC = ("'self'", "https://cdn.jsdelivr.net/npm/")
     CSP_STYLE_SRC = ("'self'", "https://cdn.jsdelivr.net/npm/")
+else:
+    CSP_SCRIPT_SRC = ("'self'")
+    CSP_STYLE_SRC = ("'self'")
 
-CSP_INCLUDE_NONCE_IN = ("script-src", "style-src")
 CSP_IMG_SRC = ("'self'", 'data:')
 CSP_OBJECT_SRC = ("'none'",)
 CSP_FRAME_SRC = ("'none'",)
